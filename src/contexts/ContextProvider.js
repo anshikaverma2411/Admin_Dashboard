e0caac5bca52d3a387d3f3c477ef1406245f1ffc
Anshika Verma <anshikaverma2411@gmail.com>
2025-04-01 19:09:10 +0530

    Added more functions to Sidebar

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <StateContext.Provider
+      value={{
+        // currentColor,
+        // currentMode,
+        activeMenu,
+        // screenSize,
+        // setScreenSize,
+        // handleClick,
+        // isClicked,
+        // initialState,
+        // setIsClicked,
+        setActiveMenu,
+        //setCurrentColor,
+        // setCurrentMode,
+        // setMode,
+        // setColor,
+        // themeSettings,
+        // setThemeSettings,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
