905086120bb680de02abe450fe7b001d53104c0e
Anshika Verma <anshikaverma2411@gmail.com>
2025-04-02 09:44:57 +0530

    Ecommerce page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+//import { useStateContext } from "../contexts/ContextProvider";
+
+const Button = ({
+  //icon,
+  bgColor,
+  color,
+  //bgHoverColor,
+  size,
+  text,
+  borderRadius,
+  //width,
+}) => {
+  //const { setIsClicked, initialState } = useStateContext();
+
+  return (
+    <button
+      type="button"
+      //onClick={() => setIsClicked(initialState)}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3  hover:drop-shadow-xl `}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
